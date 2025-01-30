7f0378d8f8cdcdb476b00f86b998229233fbb774
qkrdudwls <jacob717@ajou.ac.kr>
2025-01-30 20:54:59 +0900

    tree.h 정의

---
+++ Proteus/tree.h
@@ -0,0 +1,28 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node {
+    char *value;
+    struct Node *left;
+    struct Node *right;
+} Node;
+
+
+typedef union{
+    int integer;
+    char *string;
+    Node *node;
+} C_YYSTYPE;
+
+#define YYSTYPE C_YYSTYPE
+
+extern Node* create_node(Node *left, Node *right, char *value);
+extern void preorder(Node *node);
+extern void postorder(Node *node);
+extern void free_tree(Node *node);
+
+#endif
